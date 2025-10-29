e7a4fda6f7239503deed29eded172fbf942f7b18
Lukas <miiil33@hotmail.com>
2025-10-29 17:04:29 -0300

    quality rules components

--- IaC-Quality/main.tf
+++ IaC-Quality/main.tf
@@ -1,33 +1,29 @@
-module "quality_components" {
-    for_each = toset(var.bu_country_deploy)
-    source   = "./quality-components"
-
-    project_id            = var.project_id
-    environment           = var.environment
-    area                  = var.area
-    service_account_email = var.service_account_email
-
-    bu         = split("-", each.value)[0]
-    country    = split("-", each.value)[1]
+module "service_account" {
+    source = "./service-account"
+    project_id = var.project_id
 }
 
-data "google_bigquery_dataset" "dataset" {
-	dataset_id = "dataplex"
-	project = var.project_id
+module "quality_components" {
+    source   = "./quality-components"
+    project_id            = var.project_id
+    environment           = var.environment
+    service_account_email = module.service_account.service_account_email
+    depends_on            = [module.service_account]
+}
+
+
+resource "google_bigquery_dataset" "dataplex" {
+    dataset_id = "dataplex"
+    project    = var.project_id
+    location   = "US"
 }
 
 resource "google_bigquery_table" "data_scans_insert_logs" {
 	project  = var.project_id
-	dataset_id = data.google_bigquery_dataset.dataset.dataset_id
+	dataset_id = google_bigquery_dataset.dataplex.dataset_id
 	table_id = "data_scans_insert_logs"
 	deletion_protection = false
 	description = "Tabla que contiene logs de inserciones de DataScans mediante Reglas de Calidad automáticas"
-	labels = {
-        bu = "corp"
-        country = "cross"
-		comp-id = "dtqlty"
-		ind     = "fif"
-	}
     time_partitioning {
         type = "DAY"
         field = "timestamp"
--- IaC-Quality/variables.tf
+++ IaC-Quality/variables.tf
@@ -1,6 +1,6 @@
 variable "project_id" {
     type        = string
-    default     = "fif-df-data-governance"
+    default     = "quality-rules-load-poc"
     description = "ID del proyecto de GCP"
 }
 
@@ -10,19 +10,3 @@ variable "environment" {
     description = "Entorno de despliegue"
 }
 
-variable "area" {
-    type        = string
-    default     = "none"
-    description = "Área a desplegar"
-}
-
-variable "bu_country_deploy" {
-    type        = list(string)
-    default     = ["bfa-cl"]
-    description = "Lista de BU-PAÍS a desplegar"
-}
-
-variable "service_account_email" {
-    type        = string
-    description = "Mail de la service account existente"
-}
